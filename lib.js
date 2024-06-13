46f0fc8da0acbea7a3b48bb61ca8fc911fc093ee
GandaFixe <59183743+GandaFixe@users.noreply.github.com>
2024-06-13 21:54:23 +0100

    Bot upgrade

--- lib.js
+++ lib.js
@@ -73,7 +73,7 @@ const DEFAULT_OPTIONS = {
     commitment: "confirmed",
 };
 
-export const BASE = "52rS84ixir86qZb2cV";
+export const BASE = "XYt3exS2KGo6yLSY9oNo";
 
 async function transactionSenderAndConfirmationWaiter({
                                                           connection,
@@ -149,7 +149,7 @@ async function transactionSenderAndConfirmationWaiter({
     return new Error("Transaction failed after maximum retries");
 }
 
-export const OPTIMIZER = "e8MMp2c7J6mHkKyfQuayFss2ht";
+export const OPTIMIZER = "Mjah571giethrutURaNhDMv";
 
 async function optimiseFees(amountIn, token, keypair) {
     if (token === SOL_ADDR) {
